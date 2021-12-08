4cd3c6289794508beef690e5a59a54a289faab0a
DouglasFroes <douglassantosfroes@gmail.com>
2021-12-08 20:01:34 -0300

    relogio na tela home

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,3 +1,22 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  // relogio
+
+  Rx<DateTime> _dateTime = Rx<DateTime>(DateTime.now());
+
+  // retutn hora HH:mm
+  String get hour =>
+      _dateTime.value.hour.toString().padLeft(2, '0') +
+      ':' +
+      _dateTime.value.minute.toString().padLeft(2, '0');
+
+  void updateHour() {
+    _dateTime.bindStream(
+        Stream.periodic(Duration(minutes: 1), (_) => DateTime.now()));
+  }
+
+  HomeController() {
+    updateHour();
+  }
+}
--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:test_animete/modules/home/home_controller.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -19,27 +20,46 @@ class HomePage extends GetView<HomeController> {
             fit: BoxFit.fill,
           ),
         ),
-        child: InkWell(
-          onTap: () {
-            Get.toNamed('/menu');
-          },
-          child: Container(
-            // margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.all(5),
-            height: Get.height * 0.10,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.white30,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Text(
-              AppLocalizations.of(context)!.homeDescription,
-              style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // hora HH:mm:ss contando em tempo real
+
+            Container(
+              margin: const EdgeInsets.only(top: 150),
+              child: Obx(
+                () => Text(
+                  controller.hour,
+                  style: const TextStyle(
+                    fontSize: 50,
+                    color: Colors.white54,
+                  ),
+                ),
               ),
             ),
-          ),
+            InkWell(
+              onTap: () {
+                Get.toNamed('/menu');
+              },
+              child: Container(
+                // margin: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(5),
+                height: Get.height * 0.10,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white30,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  AppLocalizations.of(context)!.homeDescription,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
